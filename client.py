17c068f72479d04bc0d382efe1f0ef7e1a840862
TheNMD <manhdanog@gmail.com>
2022-12-06 13:53:34 +0700

    com

--- client.py
+++ client.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 HOST = socket.gethostname()
 IP = socket.gethostbyname(HOST)
-PORT = 5002
+PORT0 = 5002
+PORT1 = 5003
+PORT2 = 5004
 SERVER_IP = "192.168.1.10"
 
 BG_GRAY = "#ABB2B9"
@@ -34,7 +36,7 @@ def talkToPeer(ip):
     sktToPeer = socket.socket()
     
     try:
-        sktToPeer.connect((ip, 5003))
+        sktToPeer.connect((ip, PORT2))
     except Exception as e:
         print(e)
     
@@ -82,7 +84,7 @@ if __name__ == '__main__':
     sktServer, sktToServer = socket.socket(), socket.socket()
 
     # try:
-    #     sktToServer.connect((SERVER_IP, PORT))
+    #     sktToServer.connect((SERVER_IP, PORT1))
     #     while True:
     #         username = input("Username: ")
     #         password = input("Password: ")
@@ -94,7 +96,7 @@ if __name__ == '__main__':
     # except Exception as e:
     #     print(e)
 
-    sktServer.bind((IP, PORT))
+    sktServer.bind((IP, PORT0))
     sktServer.listen()
     t = Thread(target=listenToPeer, daemon=True)
     t.start()
@@ -108,7 +110,7 @@ if __name__ == '__main__':
     inputText = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
     inputText.grid(row=2, column=0)
     Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY, command=cmdInput).grid(row=2, column=1)
-    outputText.insert(END, "\n" + f"{HOST} : {IP} : {PORT}")
+    outputText.insert(END, "\n" + f"{HOST} : {IP} : {PORT0}")
     outputText.insert(END, "\n" + cmdInstruction())
     
     root.mainloop()
