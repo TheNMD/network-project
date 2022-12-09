3c4bfcfadabc0dafdce8ec8cc057aa3d803c3486
TheNMD <manhdanog@gmail.com>
2022-12-09 08:49:22 +0700

    com

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 import socket, time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-while True:
-    data = 'test'.encode()
-    s.sendto(data, ('255.255.255.255', 1080))
-    time.sleep(1)
+s.connect(("www.python.org", 80))
